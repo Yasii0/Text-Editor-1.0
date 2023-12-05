b0d430bc20c0626d2cd7d2861c15f2bc2b9fe2ec
Yasii0 <yasithdanthasinghe@icloud.com>
2023-12-05 17:04:32 +0530

    undo and redo feature added

--- script.js
+++ script.js
@@ -53,3 +53,11 @@ function justify(){
         })
     })
 }
+
+function undo() {
+    document.execCommand('undo', false, null);
+}
+
+function redo() {
+    document.execCommand('redo', false, null);
+}
